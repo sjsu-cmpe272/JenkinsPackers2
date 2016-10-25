b95294a8e00d001937b8470cddf43723fd8f969c
Hitesh Basantani <amrapali29>
2016-10-24 23:49:56 -0700

    Java Files added

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for(int i=1; i<=10 ; i++)
-System.out.println("Hello Amrapali "+i);
+System.out.println("Hello Amrapali...try number "+i);
 	}
 
 }
